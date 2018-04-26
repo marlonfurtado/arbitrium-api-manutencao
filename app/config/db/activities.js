3e567c7854699395a23ae6f5424cf777cf3c03f2
marlonfurtado <marlonfurtado.rs@gmail.com>
2018-04-26 00:24:17 -0300

    add cors and database scripts

---
+++ app/config/db/activities.js
@@ -0,0 +1,70 @@
+module.exports = [
+  {
+    "id": 1,
+    "description": "Dormir"
+  },
+  {
+    "id": 2,
+    "description": "Comer"
+  },
+  {
+    "id": 3,
+    "description": "Cuidados pessoais (exercícios, terapia, estética etc)"
+  },
+  {
+    "id": 4,
+    "description": "Lazer sozinho"
+  },
+  {
+    "id": 5,
+    "description": "Lazer amigos"
+  },
+  {
+    "id": 6,
+    "description": "Deslocamento de trabalho"
+  },
+  {
+    "id": 7,
+    "description": "Trabalho"
+  },
+  {
+    "id": 8,
+    "description": "Hora Extra"
+  },
+  {
+    "id": 9,
+    "description": "Estudos"
+  },
+  {
+    "id": 10,
+    "description": "Happy Hour com colegas"
+  },
+  {
+    "id": 11,
+    "description": "Viagem de trabalho"
+  },
+  {
+    "id": 12,
+    "description": "Lazer com cônjuge"
+  },
+  {
+    "id": 13,
+    "description": "Lazer com filho"
+  },
+  {
+    "id": 14,
+    "description": "Lazer com cônjuge e filho"
+  },
+  {
+    "id": 15,
+    "description": "Tarefas domésticas (limpeza, organização, reparos, supermercado)"
+  },
+  {
+    "id": 16,
+    "description": "Momento com família de origem (ex:pais, avós, tios)"
+  },
+  {
+    "id": 17,
+    "description": "Momento com a família de origem do cônjuge (ex: sogros, cunhados)"
+  }
+]
