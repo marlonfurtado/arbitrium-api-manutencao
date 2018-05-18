510abac1201872edd4e4d900029243e8a5560fed
Matheus Vaccaro <matheusvaccaro@icloud.com>
2018-05-18 18:43:35 -0300

    011 working POST /interview

---
+++ app/routes/result.js
@@ -0,0 +1,7 @@
+     var resultController = require('../controllers/result.js');
+ 
+     // Create a new result
+     //app.post('/result', resultController.create);//no need
+ 
+     // Retrieve all results
+     //app.get('/results', resultController.findAll);
