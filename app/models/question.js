e9aa1bfc2bccfe4cb79d2a54c4be76a2b01d0f74
Rafael Ritter <rafaritter44@hotmail.com>
2019-06-09 15:10:03 -0300

    Fix smells

--- app/models/question.js
+++ app/models/question.js
@@ -1,43 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
     var Question = sequelize.define('question', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
-        interview_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        event_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        question_appears_datetime: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        answered_question_datetime: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        choice: {
-            type: DataTypes.ENUM('F', 'W'),
-            allowNull: true
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: true,
-            defaultValue: DataTypes.NOW
-        },
-        updated_at: {
-            type: DataTypes.DATE,
-            allowNull: true,
-            defaultValue: DataTypes.NOW
-        }
-    }, {
-        underscored: true
-    });
-
+        id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+        interview_id: { type: DataTypes.INTEGER, allowNull: false },
+        event_id: { type: DataTypes.INTEGER, allowNull: false },
+        question_appears_datetime: { type: DataTypes.DATE, allowNull: true },
+        answered_question_datetime: { type: DataTypes.DATE, allowNull: true },
+        choice: { type: DataTypes.ENUM('F', 'W'), allowNull: true },
+        created_at: { type: DataTypes.DATE, allowNull: true, defaultValue: DataTypes.NOW },
+        updated_at: { type: DataTypes.DATE, allowNull: true, defaultValue: DataTypes.NOW }
+    }, { underscored: true });
     return Question;
 }
